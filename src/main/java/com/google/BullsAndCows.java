e2e9a0f64e02a066fa0489295f82342eb123ff32
Sankha <saheb_ray85@yahoo.co.in>
2022-02-23 22:27:02 +0530

    Bulls And Cows

---
+++ src/main/java/com/google/BullsAndCows.java
@@ -0,0 +1,70 @@
+package com.google;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+
+/**
+ * @author sanray on 2/23/2022
+ * <p>
+ * Player 1: 1351
+ * Player 2: 1234 -> bulls: 1, cows: 1
+ * Player 2: 1322 -> bulls: 2, cows: 0
+ * Player 2: 1356 -> bulls: 3, cows: 0
+ * Player 2: 3115 -> bulls: 0, cows: 4
+ * Player 2: 1111 -> bulls: 2, cows: 0
+ * Player 2: 1351 -> bulls: 4, cows: 0
+ */
+public class BullsAndCows {
+
+    public static void main(String[] args) {
+        BullsAndCows bc = new BullsAndCows();
+        System.out.println("expected [1,1] : " + bc.guess("1351", "1234"));
+        System.out.println("expected [2,0] : " + bc.guess("1351", "1322"));
+        System.out.println("expected [3,0] : " + bc.guess("1351", "1356"));
+        System.out.println("expected [0,4] : " + bc.guess("1351", "3115"));
+        System.out.println("expected [2,0] : " + bc.guess("1351", "1111"));
+        System.out.println("expected [4,0] : " + bc.guess("1351", "1351"));
+
+    }
+
+    List<Integer> guess(String player1, String player2) {
+        Map<Character, List<Integer>> m1 = getIndicesMap(player1);
+        Map<Character, List<Integer>> m2 = getIndicesMap(player2);
+
+        List<Character> bulls = new ArrayList<>();
+        Set<Integer> cows = new HashSet<>();
+        // Count bulls
+        for (Character ch : m1.keySet()) {
+            if (m2.containsKey(ch)) {
+                for (Integer index : m1.get(ch)) {
+                    if (m2.get(ch).contains(index)) {
+                        bulls.add(ch);
+                    }
+                    if (!bulls.contains(ch)) {
+                        cows.add(index);
+                    }
+                }
+            }
+        }
+        return Arrays.asList(bulls.size(), cows.size());
+    }
+
+    public static Map<Character, List<Integer>> getIndicesMap(String s) {
+        Map<Character, List<Integer>> m1 = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            Character ch = s.charAt(i);
+            if (!m1.containsKey(ch)) {
+                m1.put(ch, new ArrayList<>());
+            }
+            List<Integer> indices = m1.get(ch);
+            indices.add(i);
+        }
+        return m1;
+    }
+}
---
+++ src/main/java/com/google/Solution1.java
@@ -0,0 +1,76 @@
+package com.google;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.PriorityQueue;
+import org.apache.commons.lang3.StringUtils;
+import com.datastructure.nodedemo.Node;
+
+
+/**
+ * @author sanray on 2/22/2022
+ */
+
+public class Solution1 {
+    class Node {
+        String data;
+        int frequency;
+        Node(String data) {
+            this.data = data;
+        }
+    }
+
+    public static void main(String[] args) {
+        
+        List<String> words = Arrays.asList("apple pie",
+                "apple tree",
+                "orange tree",
+                "I like apple pie",
+                "apple store"
+                );
+        Solution1 sol1 = new Solution1();
+        HashMap<String, PriorityQueue<Node>> map = sol1.createPredictor(words);
+        predictNextWord("apple", map);
+
+    }
+
+    private static String predictNextWord(final String word, final HashMap<String, PriorityQueue<Node>> map) {
+        if(!map.containsKey(word)) {
+            return "";
+        }
+        return map.get(word).peek().data;
+    }
+
+    private HashMap<String, PriorityQueue<Node>> createPredictor(final List<String> words) {
+        if(words == null || words.isEmpty()) {
+            return null;
+        }
+
+        HashMap<String, PriorityQueue<Node>> map = new HashMap<>();
+        for(String word : words) {
+            String[] tokens = word.split(" ");
+            for(int i = 0; i < tokens.length - 1; i++) {
+                String s1 = tokens[i];
+                String s2 = tokens[i+1];
+                if(!map.containsKey(s1)) {
+                    map.put(s1, new PriorityQueue<Node>((n1, n2) ->
+                    {
+                        if (n1.data.equals(n2.data)) {
+                            n1.frequency++;
+                            return 1;
+                        }
+                        return n1.frequency > n2.frequency ? 1 : 0;
+
+                    }
+
+                    ));
+                }
+                PriorityQueue<Node> nodes = map.get(s1);
+                nodes.offer(new Node(s2));
+            }
+        }
+        return map;
+    }
+}
