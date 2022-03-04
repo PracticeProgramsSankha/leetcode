84b9b68a4284cafb8ea95eaa8caeab87523f4c42
Sankha <sanray@paypal.com>
2022-03-04 14:53:43 +0530

    longerst palindrome

---
+++ src/main/java/com/leetcode/challenges/LongestPalindrome.java
@@ -0,0 +1,40 @@
+package com.leetcode.challenges;
+
+/**
+ * https://leetcode.com/problems/longest-palindromic-substring/
+ * @author sanray on 3/4/2022
+ */
+public class LongestPalindrome {
+
+    public static void main(String[] args) {
+        LongestPalindrome sol = new LongestPalindrome();
+        System.out.println(sol.longestPalindrome("babad"));
+        System.out.println(sol.longestPalindrome("cbbd"));
+    }
+
+    private String longestPalindrome(final String inputString) {
+        if( inputString == null || inputString.isEmpty())
+            return inputString;
+        int max = 0;
+        int pos = -1;
+        String result = "";
+        for( int i = 0; i < inputString.length(); i++) {
+            int odd = getLength(inputString, i, i);
+            int even = getLength(inputString, i, i+1);
+            odd = Math.max(odd,even);
+            if( odd > max) {
+                max = odd;
+                result = inputString.substring(i-(odd-1)/2, i+odd/2+1);
+            }
+        }
+        return result;
+    }
+
+    private int getLength(String s, int start,int end) {
+        while( start >= 0 && end <= s.length()-1 && s.charAt(start) == s.charAt(end) ) {
+            --start;
+            ++end;
+        }
+        return end-start-1;
+    }
+}
