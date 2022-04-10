6e5dbacb6b92591b53bf184b45adbbe945fe15e6
SANKHA RAY <sanray@paypal.com>
2022-04-10 22:56:20 +0530

    PowXN

--- src/main/java/com/leetcode/challenges/Pow.java
+++ src/main/java/com/leetcode/challenges/Pow.java
@@ -1,6 +1,8 @@
 package com.leetcode.challenges;
 
 /**
+ * https://leetcode.com/problems/powx-n/submissions/
+ *
  * @author sanray on 10/31/2021
  */
 public class Pow {
@@ -9,52 +11,80 @@ public class Pow {
         //System.out.println(myPow(2.000000, -2147483648));
         //System.out.println(myPow( 0.44894,                 -5));
         //System.out.println(myPow( 0.999999999, -2147483648));
-        System.out.println(myPow( 2.00000, -2147483648));
+        Pow sol = new Pow();
+        System.out.println(sol.myPow(2.00000, 5));
+        System.out.println(sol.myPow(2.00000, -2147483648));
     }
 
-    public static double myPow(double x, int n) {
-
-        if(n == 0) {
+    public double pow_util(double x, long n) {
+        if (n == 0) {
             return 1.0f;
         }
-
-        if(n == 1) {
+        if (n == 1) {
             return x;
         }
 
-        if( x < -100.0f || x >= 100.0f) {
+        if (n < 0) {
+            return pow_util(1 / x, -n);
+        }
+
+        double result = pow_util(x * x, n / 2);
+        if (n % 2 == 1) {
+            result *= x;
+        }
+        return result;
+    }
+
+    public double myPow(double x, int n) {
+
+        return pow_util(x, n);
+
+    }
+
+    public static double myPow1(double x, int n) {
+
+        if (n == 0) {
+            return 1.0f;
+        }
+
+        if (n == 1) {
+            return x;
+        }
+
+        if (x < -100.0f || x >= 100.0f) {
             return 0.0f;
         }
 
-        if(n <= Integer.MIN_VALUE) {
-            if(1.0f == Math.abs(x))
+        if (n <= Integer.MIN_VALUE) {
+            if (1.0f == Math.abs(x)) {
                 return 1.0f;
+            }
           /*  else
                 return 0.0f;*/
         }
 
-        if ( n >= Integer.MAX_VALUE) {
-            if(1.0f == Math.abs(x))
+        if (n >= Integer.MAX_VALUE) {
+            if (1.0f == Math.abs(x)) {
                 return 1.0f;
-            else
+            } else {
                 return 0.0f;
+            }
 
         }
 
-
-        if( x == -1.0f || x == 0.0f || x == 1.0f) {
-            if(n == 0)
+        if (x == -1.0f || x == 0.0f || x == 1.0f) {
+            if (n == 0) {
                 return 1.0f;
-            else return x;
+            } else {
+                return x;
+            }
         }
-
-
 
         double result = 1.0f;
         boolean isPositive = n > 0;
         int power = Math.abs(n);
-        for(int i = 0; i < power ; i++) {
-            if(isPositive) {
+        for (int i = 0; i < power; i++) {
+            if (isPositive) {
                 result = result * x;
             } else {
                 result = result / x;
@@ -62,7 +92,6 @@ public class Pow {
         }
 
         return result;
-
 
     }
 }
