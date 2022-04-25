ef6577a895292eb57572e8146309f95b396675ed
SANKHA RAY <sanray@paypal.com>
2022-04-25 21:29:12 +0530

    Search Insert added tests

--- src/main/java/com/leetcode/SearchInsert.java
+++ src/main/java/com/leetcode/SearchInsert.java
@@ -13,6 +13,7 @@ public class SearchInsert {
     public static void main(String[] args) {
         SearchInsert sol = new SearchInsert();
 
+        Assert.assertEquals("TEST1", 1, sol.searchInsert(new int[] { 1, 3 }, 2));
         Assert.assertEquals("TEST1", 2, sol.searchInsert(new int[] { 1, 3, 5, 6 }, 5));
         Assert.assertEquals("TEST1", 0, sol.searchInsert(new int[] { 1, 3, 5, 6 }, 0));
         Assert.assertEquals("TEST1", 3, sol.searchInsert(new int[] { 1, 3, 5, 6 }, 6));
@@ -24,9 +25,9 @@ public class SearchInsert {
     public int searchInsert(int[] nums, int target) {
 
         int left = 0;
-        int right = nums.length;
+        int right = nums.length - 1;
 
-        while (left < right) {
+        while (left <= right) {
             int mid = left + (right - left) / 2;
             if (nums[mid] == target) {
                 return mid;
