5b94d8dc8d63c4a742dd3dcce62fe407c8c61822
SANKHA RAY <sanray@paypal.com>
2022-04-25 23:01:18 +0530

    CombinationSum

--- src/main/java/com/leetcode/challenges/CombinationSum.java
+++ src/main/java/com/leetcode/challenges/CombinationSum.java
@@ -1,22 +1,42 @@
 package com.leetcode.challenges;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Stack;
+import org.junit.Assert;
 
 
 /**
+ * https://leetcode.com/problems/combination-sum/
+ *
  * @author sanray on 12/9/2021
  */
 public class CombinationSum {
     public static void main(String[] args) {
 
         test1();
+        test2();
+
+        System.out.println("All test passed!!");
 
     }
 
     private static void test1() {
-        System.out.println(combinationSum(new int[] { 2, 3, 6, 7 }, 7));
+
+        Assert.assertEquals("TEST1",
+                Arrays.asList(Arrays.asList(2, 2, 3), Arrays.asList(7)),
+                combinationSum(new int[] { 2, 3, 6, 7 }, 7));
+    }
+
+    private static void test2() {
+
+        Assert.assertEquals("TEST1",
+                Arrays.asList(
+                        Arrays.asList(2, 2, 2, 2),
+                        Arrays.asList(2, 3, 3),
+                        Arrays.asList(3, 5)),
+                combinationSum(new int[] { 2, 3, 5 }, 8));
     }
 
     public static List<List<Integer>> combinationSum(int[] candidates, int target) {
@@ -44,6 +64,7 @@ public class CombinationSum {
         }
 
         if (candidates[index] <= target) {
+            // Repeating itself
             list.push(candidates[index]);
             backTrack(candidates, target - candidates[index], index, list, results);
             list.pop();
